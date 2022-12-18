fcb8cd06229d94a877cb6d67494c192d0ad8fce6
LordRyanII <ryanoliveiram2020@outlook.com>
2022-12-17 21:23:16 -0300

    Manipulando O resultado do localStorage
    Formatando o estilo.

--- resultado.js
+++ resultado.js
@@ -1,9 +1,9 @@
 function gerar_Curriculo(){
         //Dom
     let header = document.querySelector('#nome');
-    let date = document.querySelector('.nascimento');
-    let escola = document.querySelector('.escolaridade');
-    let cont = document.querySelector('.ctt')
+    let date = document.querySelector('.nascimento p'); //pega a classe e o paragrafo junto
+    let escola = document.querySelector('.escolaridade p');
+    let cont = document.querySelector('.ctt p')
 
     let resultado_nome = localStorage.getItem('Nome');
     let res_escolaridade = localStorage.getItem('Escolaridade');
